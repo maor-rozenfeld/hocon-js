be4754b5d5efcf89781da23f8fe52ac109fda84e
Maor Rosenfeld <maor@logz.io>
2017-03-07 08:49:01 +0200

    Use assert.equal instead of assert.ok in all tests

--- tests.js
+++ tests.js
@@ -1,107 +1,109 @@
-QUnit.test("parse basic", function( assert ) {
+QUnit.test("parse basic", function(assert) {
   var obj = parseHocon('x : 5');
-  assert.ok( obj.x === 5);
+  assert.equal(obj.x, 5);;
 });
 
-QUnit.test("parse basic in braces", function( assert ) {
+QUnit.test("parse basic in braces", function(assert) {
   var obj = parseHocon('{ x : 5 }');
-  assert.ok( obj.x === 5);
+  assert.equal(obj.x, 5);;
 });
 
-QUnit.test("parse two fields by comma", function( assert ) {
+QUnit.test("parse two fields by comma", function(assert) {
   var obj = parseHocon('{ x : 5, y : 6 }');
-  assert.ok( obj.x === 5);
-  assert.ok( obj.y === 6);
+  assert.equal(obj.x, 5);;
+  assert.equal(obj.y, 6);;
 });
 
-QUnit.test("parse two fields by newline", function( assert ) {
+QUnit.test("parse two fields by newline", function(assert) {
   var obj = parseHocon('{ x : 5\r\n y : 6 }');
-  assert.ok( obj.x === 5);
-  assert.ok( obj.y === 6);
+  assert.equal(obj.x, 5);;
+  assert.equal(obj.y, 6);;
 });
 
-QUnit.test("parse object inside another object", function( assert ) {
+QUnit.test("parse object inside another object", function(assert) {
   var obj = parseHocon('{ x { y : 6 }}');
-  assert.ok( obj.x.y === 6);
+  assert.equal(obj.x.y, 6);;
 });
 
-QUnit.test("parse float", function( assert ) {
+QUnit.test("parse float", function(assert) {
   var obj = parseHocon('x : 2.1');
-  assert.ok( obj.x === 2.1);
+  assert.equal(obj.x, 2.1);;
 });
 
-QUnit.test("parse string", function( assert ) {
+QUnit.test("parse string", function(assert) {
   var obj = parseHocon('x : a2.1');
-  assert.ok( obj.x === 'a2.1');
+  assert.equal(obj.x, 'a2.1');;
 });
 
-QUnit.test("parse deep key", function( assert ) {
+QUnit.test("parse deep key", function(assert) {
   var obj = parseHocon('x.y.z : 300');
-  assert.ok( obj.x.y.z === 300);
+  assert.equal(obj.x.y.z, 300);;
 });
 
-QUnit.test("parse deep key with object val", function( assert ) {
+QUnit.test("parse deep key with object val", function(assert) {
   var obj = parseHocon('x.y.z { a: \'hello\' , b: \'it\\\'s me\' }');
-  assert.ok( obj.x.y.z.a === 'hello');
-  assert.ok( obj.x.y.z.b === 'it\'s me');
+  assert.equal(obj.x.y.z.a, 'hello');;
+  assert.equal(obj.x.y.z.b, 'it\'s me');;
 });
 
-QUnit.test("parse basic array", function( assert ) {
+QUnit.test("parse basic array", function(assert) {
   var obj = parseHocon('[2,5]');
-  assert.ok( obj.length === 2);
-  assert.ok( obj[0] === 2);
-  assert.ok( obj[1] === 5);
+  assert.equal(obj.length, 2);;
+  assert.equal(obj[0], 2);;
+  assert.equal(obj[1], 5);;
 });
 
-QUnit.test("parse array in field", function( assert ) {
+QUnit.test("parse array in field", function(assert) {
   var obj = parseHocon('x: [2,5]');
-  assert.ok( obj.x.length === 2);
-  assert.ok( obj.x[0] === 2);
-  assert.ok( obj.x[1] === 5);
+  assert.equal(obj.x.length, 2);;
+  assert.equal(obj.x[0], 2);;
+  assert.equal(obj.x[1], 5);;
 });
 // { x {  } }
-QUnit.test("parse objects in array", function( assert ) {
+QUnit.test("parse objects in array", function(assert) {
   var obj = parseHocon('{x:[\'a\',\'b\',{c:3},5]}');
-  assert.ok( obj.x.length === 4);
-  assert.ok( obj.x[0] === 'a');
-  assert.ok( obj.x[1] === 'b');
-  assert.ok( obj.x[2].c === 3);
-  assert.ok( obj.x[3] === 5);
+  assert.equal(obj.x.length, 4);;
+  assert.equal(obj.x[0], 'a');;
+  assert.equal(obj.x[1], 'b');;
+  assert.equal(obj.x[2].c, 3);;
+  assert.equal(obj.x[3], 5);;
 });
 
-QUnit.test("parse objects in arrayx", function( assert ) {
+QUnit.test("parse objects in arrayx", function(assert) {
   var obj = parseHocon(`{x : [
   {
     c:3
   }
 ]}`);
-  assert.ok( obj.x.length === 1);
-  assert.ok( obj.x[0].c === 3);
+  assert.equal(obj.x.length, 1);;
+  assert.equal(obj.x[0].c, 3);;
 });
 
 
-QUnit.test('parse basic subtitutions', function( assert ) {
+QUnit.test('parse basic subtitutions', function(assert) {
   var obj = parseHocon('{ x: 5, y: ${x}}');
-  assert.ok(obj.x === 5);
-  assert.ok(obj.y === 5);
+  assert.equal(obj.x, 5);;
+  assert.equal(obj.y, 5);;
   console.log(obj);
 });
 
-QUnit.test('ignore comment', function( assert ) {
+QUnit.test('ignore comment', function(assert) {
   var obj = parseHocon(`{
       # this isn\'t a field
       x : 10
     }`);
-  assert.ok(obj.x === 10);
-  assert.ok(typeof obj['#'] === 'undefined');
+  assert.equal(obj.x, 10);;
+  assert.equal(typeof obj['#'], 'undefined');;
 });
 
-QUnit.test('ignore comment same line', function( assert ) {
-  var obj = parseHocon(`{
+QUnit.test('ignore comment same line', function(assert) {
+  var obj = parseHocon(
+    `{
       x : 10# this isn\'t a field
       y : 10 # this isn\'t a field
-    }`);
-  assert.ok(obj.x === 10);
-  assert.ok(obj.y === 10);
-  assert.ok(typeof obj['#'] === 'undefined');
+    }`
+  );
+  assert.equal(obj.x, 10);;
+  assert.equal(obj.y, 10);;
+  assert.equal(typeof obj['#'], 'undefined');;
 });
